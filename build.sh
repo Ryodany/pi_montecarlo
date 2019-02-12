4165c1b0855fcde526c44e96f708792a33cb8785
Ryo <ryodany@hotmail.com>
2019-02-12 20:09:19 +0100

    Fixed

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#rm -rf build && mkdir build && 
-cd build
+rm -rf build && mkdir build && cd build
 cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
 cmake --build . --config Release
